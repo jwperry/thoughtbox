dd92f9ea246f2c764401b1c84b2c1953bbf40f35
Joe Perry <joseph.w.perry@gmail.com>
2016-04-18 15:52:10 -0600

    New link form added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+  require "uri"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   helper_method :current_user
   helper_method :current_user?
+  helper_method :valid_link?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -14,4 +16,10 @@ class ApplicationController < ActionController::Base
     current_user
   end
 
+  def valid_link?(link)
+    uri = URI.parse(link)
+    uri.kind_of?(URI::HTTP)
+  rescue URI::InvalidURIError
+    false
+  end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,4 +3,20 @@ class LinksController < ApplicationController
   def index
   end
 
+  def create
+    if valid_link?(params["link"]["url"])
+      @link = Link.create(link_params)
+      flash[:notice] = "Link Created!"
+      redirect_to links_path
+    else
+      flash[:error] = "Invalid Link."
+      redirect_to links_path
+    end
+  end
+
+  private
+
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
 end
