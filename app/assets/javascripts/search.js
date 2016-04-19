3aaf449f23b013f73ad28d6bc89de1f8450291ea
Joe Perry <joseph.w.perry@gmail.com>
2016-04-18 18:58:21 -0600

    Search 95% done

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#search-bar').on("keyup", function(event) {
+    var $allLinks = $(this).parent().parent();
+    var searchTerm = this.value.toLowerCase();
+    
+    $allLinks.children().each(function (index, link) {
+      var $link = $(link);
+      var $linkTitle = $link.find('.link-title').text().toLowerCase();
+
+      if ($linkTitle.indexOf(searchTerm) !== -1) {
+        $link.show();
+      } else {
+        $link.hide();
+      }
+    });
+  });
+})
