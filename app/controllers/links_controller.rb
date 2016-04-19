8ca2783482606794ffe197dcbc9bed0c7066b7a0
Joe Perry <joseph.w.perry@gmail.com>
2016-04-18 18:31:05 -0600

    Edit functionality completed

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -16,10 +16,24 @@ class LinksController < ApplicationController
     end
   end
 
+  def edit
+    @link = Link.find(params["id"].to_i)
+  end
+
   def update
     if params["link_id"]
       link = Link.find_by(id: params["link_id"])
       link.toggle_read_status
+    else
+      if valid_link?(params["link"]["url"])
+        @link = Link.find(params["id"].to_i)
+        @link.update(link_params)
+        flash[:notice] = "Link Updated!"
+        redirect_to links_path
+      else
+        flash[:error] = "Invalid Link."
+        redirect_to links_path
+      end
     end
   end
 
